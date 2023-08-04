7e49e9cff7b6b03a87f96e182919d8c5463f4c60
arseniivetrov <arsvetrov@gmail.com>
2023-08-04 21:05:30 +0300

    refactoring

--- api_token.py
+++ api_token.py
@@ -1,3 +1,3 @@
 import telebot
 
-bot = telebot.TeleBot('6003125715:AAHuxf3xodCPNWIzHG1G3T09gVXQwgjCYHw')
+bot = telebot.TeleBot('Your API token here')
--- services.py
+++ services.py
@@ -1,8 +1,12 @@
 from scanner import Service
 
 # Services initializing Name, URL, ping_time seconds
-p2h = Service('P2H', 'https://p2h.com/', 3)
-mena = Service('MENA', 'https://google.com', 3)
+
+service1 = Service('service1_name', 'service1_url', 60)
+service2 = Service('service2_name', 'service2_url', 60)
+# Your next service here
+
 
 # Services list for thread processing
-services_list = [p2h, mena]
+
+services_list = [service1, service2, ]  # Add your next service in list
