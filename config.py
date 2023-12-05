ddd74a57d84ecf48b62c9d15c7a0acf829079664
arseniivetrov <arsvetrov@gmail.com>
2023-12-05 19:29:05 +0200

    erase_api_token

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import telebot
 from bot import Service
 
 # Telegram API token
-BOT = telebot.TeleBot('6003125715:AAHuxf3xodCPNWIzHG1G3T09gVXQwgjCYHw')
+BOT = telebot.TeleBot('api_token_here')
 
 # Services list initializing Name, URL, ping_timeout seconds
 services_list = [
